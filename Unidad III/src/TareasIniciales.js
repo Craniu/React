d435f83f3e9104c5d9cff3366f670531e89ee734
Cristoffer Venega D. <cvenegad@ccaf.andes>
2025-01-26 11:52:23 -0300

    finish Unidad III

--- Unidad III/src/TareasIniciales.js
+++ Unidad III/src/TareasIniciales.js
@@ -10,5 +10,9 @@ completada: false,
 {
 nombre: "Tarea 3",
 completada: true,
+},
+{
+    nombre: "Tarea 4",
+    completada: true,
 }
 ]
--- Unidad III/src/components/Tareas.jsx
+++ Unidad III/src/components/Tareas.jsx
@@ -1,38 +1,52 @@
 import { useState } from "react";
-import TareasIniciales from "../TareasIniciales"
+import {tareasIniciales} from "../TareasIniciales";
 
 const Tareas = () => {
+  const [entrada, setEntrada] = useState("");
+  const [tareas, setTareas] = useState(tareasIniciales);
 
-  const [entrada,setEntrada] = useState("");
-  const [tareas,setTareas] = useState(TareasIniciales);
-
-  const capturaInput = (e) =>{
-      setEntrada(e.target.value);
-      console.log(entrada);
-  }
+  const capturaInput = (e) => {
+    setEntrada(e.target.value);
+    console.log(entrada);
+  };  
 
   const enviarForm = (e) => {
     // e.preventDefault();
-    setTareas([...tareas,{nombre:entrada,completada:false}]);
+    setTareas([...tareas, { nombre: entrada, completada: false }]);
     setEntrada("");
+  };
+
+  const completarTarea = (tarea) => {
+      const nuevasTareas = [...tareas];
+      const index = nuevasTareas.findIndex(el => el.nombre === tarea.nombre)
+      nuevasTareas[index].completada = true;
+      setTareas(nuevasTareas);
   }
 
+  const eliminarTarea = (tarea) => {
+    const listaFiltrada = tareas.filter(el => el.nombre !== tarea.nombre);
+    setTareas(listaFiltrada)
+  }
 
   return (
-  <>
-    {/* <form onSubmit={enviarForm}>
+    <>
       <input type="text" onChange={capturaInput} value={entrada}></input>
-      <button>Agregar</button>
-    </form> */}
-    <input type="text" onChange={capturaInput} value={entrada}></input>
-    <button onClick={enviarForm}>Agregar</button>
-    <div className="Lista">
-    <ul>
-      {tareas.map(tarea => <li key={tarea}>{tarea}</li>)}
-    </ul>
-    </div>
-  </>
-  )
-}
+      <button onClick={enviarForm}>Agregar</button>
+      <div className="Lista">
+        <ul>
+          {tareas.map((tarea) => (
+            <li 
+            key={tarea.nombre}
+            style={tarea.completada === true ? { textDecoration:'line-through' } : {}}>
+            {tarea.nombre}
+            {tarea.completada === false ? <button onClick={()=> completarTarea(tarea)}>Completar</button>:''}
+            <button onClick={() => eliminarTarea(tarea)} >Eliminar</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Tareas;
