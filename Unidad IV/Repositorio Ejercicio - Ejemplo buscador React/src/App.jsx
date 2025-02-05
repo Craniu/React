062f4e1641885cb9872bc4fc9bd3448ed65f6852
Cristoffer Venega D. <cvenegad@ccaf.andes>
2025-02-05 12:54:20 -0300

    Fin Unidad IV

---
+++ Unidad IV/Repositorio Ejercicio - Ejemplo buscador React/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <h1>Buscador</h1>
+      <Search />
+    </>
+  );
+}
+
+export default App;
--- Unidad IV/src/App.jsx
+++ Unidad IV/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus()
+  },[])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ejemplo useRef - Focus automático</h1>
+      <input type="text" placeholder="Nombre" ref={inputRef}/>
+      <input type="text" placeholder="Correo electrónico" />
+      <input type="text" placeholder="Teléfono" />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
